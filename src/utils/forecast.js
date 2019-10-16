66a0f475294882d45a1e7130443d11cdc716c36b
Abe Anderson <aberanderson@gmail.com>
2019-10-16 14:07:55 -0400

    bunch of msg changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,8 +11,14 @@ const forecast = (lng, lat, callback) => {
             callback('Unable to find location', undefined)
         } else {
             const { temperature, precipProbability } = currently;
-            const { summary } = daily.data[0]
-            callback(undefined, `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
+            const { summary, temperatureMin, temperatureMax, humidity, percipType } = daily.data[0]
+            console.log(daily.data[0])
+            weatherData = `It is currently ${Math.round(temperature)}F degrees out \
+            with a high of ${Math.round(temperatureMax)}F and a low of ${Math.round(temperatureMin)}F.\
+            The current humidity is ${humidity * 100}%.\
+            ${summary}
+            There is a ${precipProbability * 100}% chance of ${percipType || 'perciptation'}.\ `
+            callback(undefined, weatherData)
         }
     });
 }
